5ea7238ca87c794c2e19c96c5c5fb77dce236853
benugwu <123969178+benugwu@users.noreply.github.com>
2023-03-06 06:34:25 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet - Prints the alphabet in lowercase.
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 96; c <= 122; i++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+
